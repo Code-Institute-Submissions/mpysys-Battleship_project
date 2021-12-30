79b98fdf63cb5eef77e251b9ec118383ca1f668d
mpysys <monnet.morgan@gmail.com>
2021-12-30 17:58:03 +0000

    Final commit

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from random import randint
-import os 
+import os
 
 class Board:
     """
@@ -14,7 +14,7 @@ class Board:
             self.orientation = orientation
         else:
             raise ValueError("Value must be 'horizontal' or 'vertical'.")
-        
+
         #Set coordinates of ship on board
         if orientation == 'horizontal':
             if location['row'] in range(row_length):
@@ -43,13 +43,13 @@ class Board:
             raise IndexError("A ship already occupies that space.")
         else:
             self.fillBoard()
-    
+
     def filled(self): #method to determine if position is filled by a ship
         for coords in self.coordinates:
             if board[coords['row']][coords['col']] == 1:
                 return True
         return False
-    
+
     def fillBoard(self):#method that assigns ship to coordinate
         for coords in self.coordinates:
             board[coords['row']][coords['col']] = 1
@@ -59,7 +59,7 @@ class Board:
             if coords == location:
                 return True
         return False
-  
+
     def destroyed(self): #method checks for status of ships
         for coords in self.coordinates:
             if board_display[coords['row']][coords['col']] == '.':
@@ -68,19 +68,19 @@ class Board:
                 raise RuntimeError("Board display inaccurate")
         return True
 
-#Variables for board and game set up
-turns = 40 #number of turns
+# Variables for board and game set up
+turns = 40 # number of turns
 
-row_length = 9 #number of rows
-col_length = 9 #number of columns
+row_length = 9 # number of rows
+col_length = 9 # number of columns
 
 board = [[0] * col_length for x in range(row_length)]
-board_display = [["."] * col_length for x in range(row_length)] #list comprehension to display 0's as board that will be passed as argument
+board_display = [["."] * col_length for x in range(row_length)] # list comprehension to display 0's as board that will be passed as argument
 
-ships_to_destroy = 4 #number of ships to destroy
-max_ships_size= 5 #max length of ships
-min_ships_size = 2 #min length of ships
-ship_list = [] #List that stores ship data
+ships_to_destroy = 4 # number of ships to destroy
+max_ships_size= 5 # max length of ships
+min_ships_size = 2 # min length of ships
+ship_list = [] # List that stores ship data
 
 def print_board(board_array):
     """
@@ -177,12 +177,12 @@ while temp < ships_to_destroy:
     else:
         ship_list.append(Board(ship_info['size'], ship_info['orientation'], ship_info['location']))
         temp += 1
-del temp   
+del temp
 
 def main():
     """
     Run all program functions and active states
-    """         
+    """
     os.system('clear')
     print('-'*35)
     print('Welcome to Battleships on Python!')
@@ -197,7 +197,7 @@ def main():
     print('-'*35)
     print('-'*35)
     print_board(board_display)
-    
+
     for turn in range(turns):
         print("Turn:", turn + 1, "of", turns)
         print("Ships left:", len(ship_list))
@@ -228,12 +228,12 @@ def main():
         if not ship_hit:
             board_display[guess_coords['row']][guess_coords['col']] = '#'
             print("You missed!")
-        
+
         print_board(board_display)
 
         if not ship_list:
             break
-    
+
     if ship_list:
         print("You lose!")
     else:
@@ -243,4 +243,3 @@ def main():
         print("-" * 35)
 
 main()
-
